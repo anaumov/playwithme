7916bddf5dc1ab8262e2d85fe56c725b9d961fd5
MaximShirokov <maximshirokov21@gmail.com>
2014-08-15 20:39:38 +0400

    Implemented in the form of display errors and added field login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :login, :email, :password, :password_confirmation, presence: true
+  validates :login, length: { minimum: 2 }, uniqueness: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }
+  validates :password, confirmation: true
 end
