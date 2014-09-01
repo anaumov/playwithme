41133a814256ecccf5cda511aa00c5ad9f9828c0
MaximShirokov <maximshirokov21@gmail.com>
2014-09-01 17:42:18 +0400

    Implementation approve and reject invite

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -3,13 +3,13 @@ class InvitesController < InheritedResources::Base
   def accept
     invite = Invite.find(params[:invite_id])
     invite.accept
-    redirect_to user_page(current_user)
+    redirect_to my_path
   end
 
    def reject
     invite = Invite.find(params[:invite_id])
     invite.reject
-    redirect_to user_page(current_user)
+    redirect_to my_path
    end
 
   private
