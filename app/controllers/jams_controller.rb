6a8e0f92a5d28b998420cd77d2f8a99210bb222a
MaximShirokov <maximshirokov21@gmail.com>
2014-09-03 13:54:55 +0400

    Limit rights of users to jams in controller

--- app/controllers/jams_controller.rb
+++ app/controllers/jams_controller.rb
@@ -1,9 +1,29 @@
 class JamsController < InheritedResources::Base
 
   before_action :authenticate_user!, only: [:new, :create, :update, :destroy]
+  before_filter :find_jam , only: [:show, :edit, :update, :destroy]
 
   def new
     @jam = current_user.own_jams.new
+  end
+
+  def edit
+    authorize_action_for(@jam)
+  end
+
+  def update
+    authorize_action_for(@jam)
+    if @jam.update_attributes(permitted_params[:jam])
+      redirect_to jams_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    authorize_action_for(@jam)
+    @jam.destroy
+    redirect_to jams_path
   end
 
   def invite
@@ -18,6 +38,10 @@ class JamsController < InheritedResources::Base
 
     def permitted_params
       params.permit!
+    end
+
+    def find_jam
+      @jam = Jam.find(params[:id])
     end
 
 end
